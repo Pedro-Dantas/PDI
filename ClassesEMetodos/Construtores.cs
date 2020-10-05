5e3f12236c707f3d5f1aed37a7de5725cb411eb7
Pèdro Dantas <dev.pedrodantas@gmail.com>
2020-10-05 14:58:10 -0300

    Adicionado novas formas de se instânciar

--- ClassesEMetodos/Construtores.cs
+++ ClassesEMetodos/Construtores.cs
@@ -9,6 +9,20 @@ namespace CursoCSharp.ClassesEMetodos
         public string Modelo;
         public string Fabricante;
         public int Ano;
+
+        public Carro() { }
+
+        public Carro(string modelo, string fabricante, int ano) 
+        {
+            Modelo = modelo;
+            Fabricante = fabricante;
+            Ano = ano;
+        }
+
+        public void Imprimir() 
+        {
+            Console.WriteLine($"{Fabricante} {Modelo} {Ano}");
+        }
     }
 
     class Construtores
@@ -19,9 +33,18 @@ namespace CursoCSharp.ClassesEMetodos
             carro1.Fabricante = "BMW";
             carro1.Modelo = "325i";
             carro1.Ano = 2017;
+            carro1.Imprimir();
+            
+            Carro carro2 = new Carro("Uno", "Fiat", 2017);
+            carro2.Imprimir();
 
-            Console.WriteLine($"{carro1.Fabricante} {carro1.Modelo} {carro1.Ano}");
-
+            Carro carro3 = new Carro() 
+            { 
+                Modelo = "Ka",
+                Fabricante = "Fiat",
+                Ano = 2015
+            };
+            carro3.Imprimir();
         }
     }
 }
