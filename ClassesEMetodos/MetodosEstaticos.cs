a262250b1cd82b5900b0e60f31ec76f7b3c47905
Pèdro Dantas <dev.pedrodantas@gmail.com>
2020-10-05 21:26:52 -0300

    Métodos de classe e de objeto

---
+++ ClassesEMetodos/MetodosEstaticos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    public class CalculadoraEstatica
+    {
+        // Método de instância
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Método de classe ou estático
+        public static int Multiplcar(int a, int b) 
+        {
+            return a * b;
+        }
+    }
+
+    class MetodosEstaticos
+    {
+        public static void Executar() 
+        {
+            var resultado = CalculadoraEstatica.Multiplcar(2, 2);
+            Console.WriteLine(resultado);
+
+            CalculadoraEstatica calculadoraEstatica = new CalculadoraEstatica();
+
+            Console.WriteLine(calculadoraEstatica.Somar(2, 2));
+        }
+    }
+}
